b9e25b9db43c5d36c3b6e8df92ba88ad589a74b8
Tsvetelin Kutsarov <tkutsarov@yahoo.com>
2015-04-27 23:59:03 +0300

    connection change

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
     $server = 'localhost';
-    $dbUsername = 'codestor';
-    $dbPassword = 'wIKb+)6n8RbD';
-    $database = 'codestor_forum_test';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $database = 'forum_test';
     $conn = new mysqli($server, $dbUsername, $dbPassword, $database);
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
