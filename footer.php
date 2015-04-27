07384db74daabcd4b95f7d4c733e416f0a88e091
tkutsarov <tkutsarov@yahoo.com>
2015-04-27 20:24:42 +0300

    footer fix

--- footer.php
+++ footer.php
@@ -1,20 +1,18 @@
 
-</main><!-- content -->
+    </main><!-- content -->
 
-</div><!-- wrapper -->
+    </div><!-- wrapper -->
 
-<footer id="footer">
-
-
-    <div>
-        <p> PHP team project &copy</p>
-        <p>Github :
-            <a  href=
-                "https://github.com/tkutsarov/PHP_Team_Seattle" target="_blank">
-                <img src="images/github_img.png" id="footSprite"/>
-            </a></p>
-    </div>
-</footer>
+    <footer id="footer">
+        <div id="footer-div">
+            <p> PHP team project &copy</p>
+            <p>Github :
+                <a  href="https://github.com/tkutsarov/PHP_Team_Seattle" target="_blank">
+                    <img src="images/github_img.png" id="footSprite"/>
+                </a>
+            </p>
+        </div>
+    </footer>
 </body>
 </html>
 
