6f5c5d31ef34bcbd1efbed43eaf6480f7a3dba6e
booster085 <iliangrekov@abv.bg>
2015-04-28 08:55:30 +0300

    Fixed search button responsive problem

--- search_form.php
+++ search_form.php
@@ -1,7 +1,11 @@
 <div id="search-wrapper">
     <form method="POST" action="search_topic.php?go" id="search-form">
+        <div id="search-field-box">
         <input type="search" id="search-box" placeholder="search.." name="keywords"/>
+        </div>
+        <div id="search-btn-box">
         <button type="submit" name="search" id="search-btn"></button>
+            </div>
     </form>
 </div>
 <div style="clear:both;"></div>
